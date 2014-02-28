657ca590888b59bb57f527ed2b33b9ced8b574c2
Farzana Meru <farzana.meru@phys.ethz.ch>
2014-02-28 12:11:16 +0100

    Creating a conflict

--- Gregor.py
+++ Gregor.py
@@ -4,5 +4,5 @@
 print "Gregor says hi!"
 print ' veloxid says Bu'
 =======
-print "Gregor says hi to everbody"
+print "Gregor says hello to everbody"
 >>>>>>> eaf6d4061aace354cc74db2af241e39242cc7343
